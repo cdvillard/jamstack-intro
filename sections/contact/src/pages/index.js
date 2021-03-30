00b18fdac4e2908262a0a9c4fe768c86f576927d
Charles Villard <c.d.villard@gmail.com>
2021-03-30 01:52:45 -0400

    feat(contact): install gatsby and setup form

---
+++ sections/contact/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/form";
+
+import "../global.css";
+
+const ContactPage = () => (
+  <>
+    <h1>Send a message!</h1>
+    <Form />
+  </>
+);
+
+export default ContactPage;
